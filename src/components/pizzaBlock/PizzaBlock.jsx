0d6ac3fea84768e01c7a1966ba1cdaec17fe810e
elivanov.denis <elivanov.denis@mediasoft.team>
2020-11-14 18:16:14 +0400

    change PizzaBlock

--- src/components/pizzaBlock/PizzaBlock.jsx
+++ src/components/pizzaBlock/PizzaBlock.jsx
@@ -50,37 +50,36 @@ const PizzaBlock = ({id, name, types, imageUrl, sizes, price, onAddItem}) => {
     }
 
     return (
-        <div className='pizza-block'>
+        <div className="pizza-block">
             <img
-                className='pizza-block__image'
+                className='pizza-block_img'
                 src={imageUrl}
                 alt='pizza'
             />
-            <h4 className='pizza-block__title'>{name}</h4>
-            <div className='pizza-block__selector'>
+            <h3 className="pizza-block_name">{name}</h3>
+            <div className="pizza-block_section">
                 <ul>
                     {selectTypeOfItem(availableTypes, selectedType, types, setSelectedType, true)}
                 </ul>
                 <ul>
-                    {selectTypeOfItem(availableSizes, selectedSize, sizes, setSelectedSize, false)}
+                    {selectTypeOfItem(availableSizes.map(el => `${el} см.`), selectedSize, sizes, setSelectedSize, false)}
                 </ul>
             </div>
-            <div className='pizza-block__bottom'>
-                <div className='pizza-block__price'>от {pricePizza} ₽</div>
-                <Button className='button button--add' outline onClick={handleAddItem}>
-                    <svg
-                        width='12'
-                        height='12'
-                        viewBox='0 0 12 12'
-                        fill='none'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
+            <div className="pizza-block__bot">
+                <div className="pizza-block__bot_price">
+                    от {pricePizza} ₽
+                </div>
+                <Button class="button button--add" outline onClick={handleAddItem}>
+                    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
-                            d='m10.8 4.8h7.2v1.2c7.2 0.5373 6.6627 0 6 0c5.3373 0 4.8 0.5373 4.8 1.2v4.8h1.2c0.5373 4.8 0 5.3373 0 6c0 6.6627 0.5373 7.2 1.2 7.2h4.8v10.8c4.8 11.4627 5.3373 12 6 12c6.6627 12 7.2 11.4627 7.2 10.8v7.2h10.8c11.4627 7.2 12 6.6627 12 6c12 5.3373 11.4627 4.8 10.8 4.8z'
-                            fill='white'
-                        />
+                            d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                            fill="#EB5A1E"/>
+                        <path
+                            d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                            fill="#EB5A1E"/>
                     </svg>
-                    <span>добавить</span>
+
+                    <span>Добавить</span>
                     {countPizza && <i>{countPizza}</i>}
                 </Button>
             </div>
