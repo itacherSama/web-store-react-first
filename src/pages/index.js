8191fd97f368c007b42caf9e26a3e06587aefa24
elivanov.denis <elivanov.denis@mediasoft.team>
2020-11-25 17:51:41 +0400

    add index for pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-export { default as Home } from './Home/Home';
-export { default as Cart } from './Cart/Cart';
-export { default as NotFound } from './NotFound/NotFound';
+export { default as NotFound } from './NotFound';
+export { default as Home } from './Home';
+export { default as Cart } from './Cart';
+
