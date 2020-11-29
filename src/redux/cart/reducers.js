9e5e95b1876621dc8a157e502d985dd73b6f0ddd
elivanov.denis <elivanov.denis@mediasoft.team>
2020-11-29 12:43:51 +0400

    fix property in icon and delete info for test

--- src/redux/cart/reducers.js
+++ src/redux/cart/reducers.js
@@ -2,52 +2,8 @@ import actionTypes from './types';
 
 import { deletePizza, performOperationsOnElement } from './utils';
 
-const pizzas = {
-  '3': [
-    {
-      item: {
-        id: 3,
-        name: 'Кисло-сладкий цыпленок',
-        imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg',
-        price: 510,
-        size: 40,
-        type: 'традиционное'
-      },
-      totalItems: 1,
-      totalPrice: 510
-    },
-    {
-      item: {
-        id: 3,
-        name: 'Кисло-сладкий цыпленок',
-        imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg',
-        price: 375,
-        size: 30,
-        type: 'традиционное'
-      },
-      totalItems: 3,
-      totalPrice: 1125
-    }
-  ],
-  '5': [
-    {
-      item: {
-        id: 5,
-        name: 'Крэйзи пепперони',
-        imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg',
-        price: 720,
-        size: 30,
-        type: 'тонкое'
-      },
-      totalItems: 3,
-      totalPrice: 2160
-    }
-  ]
-};
-
-
 const initialState = {
-  items: { ...pizzas },
+  items: {},
   totalPrice: 0,
   totalItems: 0
 };
@@ -80,3 +36,47 @@ const cartReducer = (state = initialState, action) => {
 export default cartReducer;
 
 
+
+// for tests 
+// const pizzas = {
+//   '3': [
+//     {
+//       item: {
+//         id: 3,
+//         name: 'Кисло-сладкий цыпленок',
+//         imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg',
+//         price: 510,
+//         size: 40,
+//         type: 'традиционное'
+//       },
+//       totalItems: 1,
+//       totalPrice: 510
+//     },
+//     {
+//       item: {
+//         id: 3,
+//         name: 'Кисло-сладкий цыпленок',
+//         imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg',
+//         price: 375,
+//         size: 30,
+//         type: 'традиционное'
+//       },
+//       totalItems: 3,
+//       totalPrice: 1125
+//     }
+//   ],
+//   '5': [
+//     {
+//       item: {
+//         id: 5,
+//         name: 'Крэйзи пепперони',
+//         imageUrl: 'https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg',
+//         price: 720,
+//         size: 30,
+//         type: 'тонкое'
+//       },
+//       totalItems: 3,
+//       totalPrice: 2160
+//     }
+//   ]
+// };
