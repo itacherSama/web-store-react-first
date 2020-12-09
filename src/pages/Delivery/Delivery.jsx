31d1f07fd366583c0c66048395b28a79026922bd
elivanov.denis <elivanov.denis@mediasoft.team>
2020-12-09 12:26:46 +0400

    change page delivery

--- src/pages/Delivery/Delivery.jsx
+++ src/pages/Delivery/Delivery.jsx
@@ -3,26 +3,53 @@ import { useDispatch } from 'react-redux';
 import { setDelivery } from '@redux/form/actions';
 // import { Link } from 'react-router-dom';
 
+import MapYandex from '@components/MapYandex';
+import Button from '@components/Button';
 import DeliveryForm from './DeliveryForm';
 
 import styles from './Delivery.module.scss';
 
+const typeContent = ['formDelivery', 'mapDelivery'];
+
 const Delivery = () => {
   const dispatch = useDispatch();
+  const [content, setContent] = React.useState(typeContent[0]);
 
   const submitForm = (valuesOfData) => {
     dispatch(setDelivery(valuesOfData));
   };
 
+  const changeContentDelivery = () => {
+    const newContent = content === typeContent[0] ? typeContent[1] : typeContent[0];
+    setContent(newContent);
+  };
+
+  const СontainerDeliveryForm = (
+    <div>
+      <div className={ styles.description }>
+        Введите адрес или выберите дом
+        <span className={ styles.descriptionMap } onClick={ changeContentDelivery }> на карте</span>
+      </div>
+      <DeliveryForm submitForm={ submitForm } />
+    </div>
+  );
+
+  const СontainerDeliveryMap = (
+    <div>
+      <div className={ styles.button }>
+        <Button className={ 'buttonBack' } onClick={ changeContentDelivery }>Назад</Button>
+      </div>
+      <MapYandex />
+    </div>
+  );
+
   return (
     <div className={ styles.container } >
-      <div className={ styles.title }>Адрес</div>
-      <div className={ styles.description }>
-        Введите адрес или выберите дом <ins> на карте</ins>
-      </div>
-      <div className="form">
-        {<DeliveryForm submitForm={ submitForm } />}
-      </div>
+      <h2 className={ styles.title }>Адрес</h2>
+
+      {content === typeContent[0]
+        ? СontainerDeliveryForm
+        : СontainerDeliveryMap }
     </div>
   );
 };
